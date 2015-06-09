34aeb8e1f74110497ef442e5fd70c2ebb934b662
reecej662 <reecej662@gmail.com>
2015-06-08 21:41:26 -0700

    Added login feature

---
+++ SwimSchedulerApp/ParseStarterProject/LoginViewController.swift
@@ -0,0 +1,175 @@
+//
+//  LoginViewController.swift
+//  SwimScheduler
+//
+//  Created by Reece Jackson on 6/8/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    var signupActive = true
+    
+    @IBOutlet var username: UITextField!
+    
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var signupButton: UIButton!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    @IBOutlet var registeredText: UILabel!
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    func displayAlert(title: String, message: String) {
+        
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction((UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        })))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func signUp(sender: AnyObject) {
+        
+        if username.text == "" || password.text == "" {
+            
+            displayAlert("Error in form", message: "Please enter a username and password")
+            
+        } else {
+            
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            if signupActive == true {
+                
+                
+                var user = PFUser()
+                user.username = username.text
+                user.password = password.text
+                
+                var errorMessage = "Please try again later"
+                
+                user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    if error == nil {
+                        
+                        //signup successful
+                        
+                        self.performSegueWithIdentifier("login", sender: self)
+                        
+                    } else {
+                        
+                        if let errorString = error!.userInfo?["error"] as? String {
+                            
+                            errorMessage = errorString
+                            
+                        }
+                        
+                        self.displayAlert("Failed SignUp", message: errorMessage)
+                        
+                    }
+                    
+                })
+                
+            } else {
+                
+                PFUser.logInWithUsernameInBackground(username.text, password: password.text, block: { (user, error) -> Void in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    var errorMessage = "Please try again later"
+                    
+                    if user != nil {
+                        
+                        // Logged in!
+                        
+                        self.performSegueWithIdentifier("login", sender: self)
+                        
+                    } else {
+                        
+                        if let errorString = error!.userInfo?["error"] as? String {
+                            
+                            errorMessage = errorString
+                            
+                        }
+                        
+                        self.displayAlert("Failed Log In", message: errorMessage)
+                        
+                    }
+                    
+                })
+                
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func logIn(sender: AnyObject) {
+        
+        if signupActive == true {
+            
+            signupButton.setTitle("Log In", forState: UIControlState.Normal)
+            
+            registeredText.text = "Not registered?"
+            
+            loginButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            
+            signupActive = false
+            
+        } else {
+            
+            signupButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            
+            registeredText.text = "Already registered?"
+            
+            loginButton.setTitle("Login", forState: UIControlState.Normal)
+            
+            signupActive = true
+            
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
