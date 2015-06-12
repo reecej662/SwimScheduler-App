04f8c0261a3b85ca170d764ea934d48efdd65b34
reecej662 <reecej662@gmail.com>
2015-06-12 13:16:55 -0700

    Added pages to select lesson details

--- SwimSchedulerApp/ParseStarterProject/NewLessonViewController.swift
+++ SwimSchedulerApp/ParseStarterProject/NewLessonViewController.swift
@@ -29,7 +29,7 @@ class NewLessonViewController: UIViewController, UITableViewDelegate, UITableVie
     
     @IBAction func submit(sender: AnyObject) {
         
-        //if this fucker presses submit add a lesson and shit
+        //when the submit button is pressed set the values and save the new lesson
         var lesson = PFObject(className: "lessons")
         lesson["date"] = date.date
         lesson["clientId"] = "Implement later"
@@ -45,17 +45,6 @@ class NewLessonViewController: UIViewController, UITableViewDelegate, UITableVie
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using se gue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     //Functions for the table view below the date picker
     
@@ -79,6 +68,7 @@ class NewLessonViewController: UIViewController, UITableViewDelegate, UITableVie
         cell.textLabel!.text = lessonOptions[indexPath.row]
         
         cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
         return cell
         
     }
@@ -89,65 +79,20 @@ class NewLessonViewController: UIViewController, UITableViewDelegate, UITableVie
         return false
     }
     
-    /*
-    func tableView(tableView:UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == UITableViewCellEditingStyle.Delete {
-            // handle delete (by removing the data from your array and updating the tableview)
-            
-            var deleteId = "" //clientIds[indexPath.row]
-            
-            var query = PFQuery(className: "clients")
-            
-            query.getObjectInBackgroundWithId(deleteId, block: { (object, error) -> Void in
-                if error == nil && object != nil{
-                    object!.deleteInBackground()
-                } else {
-                    println(error)
-                }
-            })
-            
-            //clientIds.removeAtIndex(indexPath.row)
-            //firstNames.removeAtIndex(indexPath.row)
-            //lastNames.removeAtIndex(indexPath.row)
-            
-            //self.tableView.reloadData()
-        } else if editingStyle == UITableViewCellEditingStyle.Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-    /*
-    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-    
-    
-    
-    }
-    */
-    
     func tableView(tableView:UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         lessonOptionsTable.deselectRowAtIndexPath(indexPath, animated: true)
         
         //selectedClient = clientIds[indexPath.row]
         //self.performSegueWithIdentifier("clientInfo", sender: self)
+    
+        if indexPath.row == 0 {
+            performSegueWithIdentifier("chooseClient", sender: self)
+            
+        } else if indexPath.row == 1 {
+            performSegueWithIdentifier("chooseTime", sender: self)
+            
+        }
     }
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-    
-    }
-    */
-    
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return NO if you do not want the item to be re-orderable.
-    return true
-    }
-    */
-    
-    
-    // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -159,6 +104,10 @@ class NewLessonViewController: UIViewController, UITableViewDelegate, UITableVie
             var clientPage = segue.destinationViewController as! ClientInfoViewController
             
             //clientPage.clientId = selectedClient
+            
+        } else if segue.identifier == "chooseClient" {
+            
+            
             
         }
     }
